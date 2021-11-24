c0e2915ace430dcd24fef4d4a1c41489760a5665
Koen Siteur <k.siteur@gmail.com>
2021-11-24 15:17:13 +0100

    final photofinish

--- finishphoto/sketch.js
+++ finishphoto/sketch.js
@@ -1,23 +1,28 @@
 let capture;
-var w=1;
-var x=1;
+var x=0;
 function setup() {
-  createCanvas(1600, windowHeight);
+  createCanvas(640, 480);
   let constraints = {
     video: true,
     audio: false
   };
   
-  capture = createCapture(constraints);
+  capture = createCapture(constraints,ready);
   capture.hide();
   frameRate(60)
 }
+var go=false;
+function ready(){
+  go=true;
+}
 
 function draw() {
-  copy(capture,width/2,0,1,height,x,0,1,height);
-  x=x+w;
+  if (go){
+    copy(capture,width/2,0,1,height,x,0,1,height);
+      x=x+1;
+    if (x==width){
+      noLoop();
+    }
+  }
 }
 
-function windowResized() {
-  resizeCanvas(1600, windowHeight);
-}
